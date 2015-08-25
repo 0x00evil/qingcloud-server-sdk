eaaa3a616cc4314adbc28cce496cde07e9b93132
gaoyuehua <gaoyuehua@happysoft.cc>
2015-08-25 19:20:11 +0800

    实现了RunInstances接口

--- lib/client/service.rb
+++ lib/client/service.rb
@@ -14,6 +14,28 @@ module QingCloudServer
             zone:
           )eval(make_action_and_params)
       end
+
+      def run_instances(
+            image_id:,
+            instance_type: nil,
+            cpu: nil,
+            memory: nil,
+            count: 1,
+            instance_name: nil,
+            login_mode:,
+            login_pair: nil,
+            login_passwd: nil,
+            vxnets_N: [],
+            security_group: nil,
+            volumes_N: [],
+            need_newsid: nil,
+            need_userdata: nil,
+            user_data_type: nil,
+            user_data_value: nil,
+            user_data_path: nil,
+            user_data_file: nil,
+            zone:)eval(make_action_and_params)
+      end
     end
   end
 end
