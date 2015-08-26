6a879f271526cbd69d08b3507d8afed17121cbb5
gaoyuehua <gaoyuehua@happysoft.cc>
2015-08-26 18:25:33 +0800

    open Array and add a method

--- lib/client/service_foudation.rb
+++ lib/client/service_foudation.rb
@@ -1,3 +1,4 @@
+# must in the top-level namespace
 class Array
   def arr_to_hsh
     hsh = {}
@@ -23,17 +24,17 @@ module QingCloudServer
         self.response = @connector.fetch_server_condition(action, params)
       end
 
-      def convert_arr_to_hash(arr)
-        hash = {}
-        arr.each {|h| hash.merge!(h)}
-        hash
-      end
+      # def convert_arr_to_hash(arr)
+      #   hash = {}
+      #   arr.each {|h| hash.merge!(h)}
+      #   hash
+      # end
 
       def make_action_and_params
         "
         fetch_response(
             __method__.to_s.split('_').map {|e| e.capitalize!}.join,
-            eval('convert_arr_to_hash(method(__method__).parameters.map { |_, p| {p.to_sym => eval(p.to_s)} })')
+            method(__method__).parameters.map { |_, p| {p.to_sym => eval(p.to_s)} }.arr_to_hsh
         )
         "
       end
